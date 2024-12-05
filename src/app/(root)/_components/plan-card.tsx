90d5f522abe4eb559ced525f4532f460b7d25f92
Joshua <josuealejandrof926@gmail.com>
2024-12-05 23:24:56 +0100

    se ve cool la verda

--- src/app/(root)/_components/plan-card.tsx
+++ src/app/(root)/_components/plan-card.tsx
@@ -1,3 +1,4 @@
+import { BorderBeam } from '@/components/ui/border-beam';
 import { Button } from '@/components/ui/button';
 import {
   Card,
@@ -7,37 +8,39 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { Separator } from '@/components/ui/separator';
 import { Plan } from '@/lib/interfaces';
 import { CheckIcon, XIcon } from 'lucide-react';
 
 export const PlanCard = ({ plan }: { plan: Plan }) => {
   return (
-      <Card className="max-w-md shadow-lg hover:shadow-primary/40 transition-all shadow-primary/0 flex flex-col items-stretch justify-start">
-        <CardHeader className="space-y-6">
-          <CardTitle className="text-lg text-foreground/60">
-            {plan.name}
-          </CardTitle>
+    <Card className="w-full sm:max-w-md shadow-lg hover:shadow-primary/40 transition-all shadow-primary/0 flex flex-col items-stretch justify-start relative group overflow-clip">
+      <CardHeader className="space-y-6">
+        <CardTitle className="text-lg text-foreground/60">
+          {plan.name}
+        </CardTitle>
 
-          <h1 className="text-6xl font-bold">
-            {typeof plan.price.base === 'string'
-              ? plan.price.base
-              : plan.price.base.toLocaleString('es-ES', {
-                  style: 'currency',
-                  currency: 'EUR',
-                  maximumFractionDigits: 0,
-                })}
-            <span className="text-base text-foreground/60">
-              {plan.price.units}
-            </span>
-          </h1>
-          <Separator />
-          <CardDescription className="text-lg leading-tight !mt-2">
-            {plan.description}
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col justify-start items-center grow">
-          <ul className="flex flex-col gap-y-4">
+        <h1 className="text-6xl font-bold">
+          {typeof plan.price.base === 'string'
+            ? plan.price.base
+            : plan.price.base.toLocaleString('es-ES', {
+                style: 'currency',
+                currency: 'EUR',
+                maximumFractionDigits: 0,
+              })}
+          <span className="text-base text-foreground/60">
+            {plan.price.units}
+          </span>
+        </h1>
+        <Separator />
+        <CardDescription className="text-lg leading-tight !mt-2">
+          {plan.description}
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col justify-start items-center grow">
+        <ScrollArea className="h-72 pr-4">
+          <ul className="flex flex-col gap-y-4 max-h-64">
             {plan.features &&
               plan.features.map((feature) => (
                 <li
@@ -67,16 +70,20 @@ export const PlanCard = ({ plan }: { plan: Plan }) => {
                 </li>
               ))}
           </ul>
-        </CardContent>
-        <CardFooter>
-          <Button
-            variant="outline"
-            className="w-full hover:bg-primary hover:text-primary-foreground  "
-          >
-            {plan.buy || 'Empieza ahora'}
-          </Button>
-        </CardFooter>
-      </Card>
-
+        </ScrollArea>
+      </CardContent>
+      <CardFooter>
+        <Button
+          variant="outline"
+          className="w-full hover:bg-primary hover:text-primary-foreground  "
+        >
+          {plan.buy || 'Empieza ahora'}
+        </Button>
+      </CardFooter>
+      <BorderBeam
+        delay={Math.random() * 2 + 5}
+        duration={Math.random() * 6 + 3}
+      />
+    </Card>
   );
 };
--- src/app/(root)/pricing/page.tsx
+++ src/app/(root)/pricing/page.tsx
@@ -5,9 +5,9 @@ export default function PlanesPage() {
 
   return (
     <main className="grow flex flex-col items-center justify-start py-16 px-8 gap-y-12">
-      <h1 className="text-6xl font-bold">Lo que podemos ofrecerte</h1>
+      <h1 className="text-6xl font-bold">¿Que podemos ofrecerte?</h1>
 
-      <div className="flex flex-row gap-x-8 flex-wrap gap-y-8">
+      <div className="flex flex-row gap-x-8 flex-wrap gap-y-8 items-start justify-center">
         {plans.map((plan) => (
           <PlanCard
             key={plan.name}
